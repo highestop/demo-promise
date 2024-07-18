074727650b33ab9e05e775ece2e7a231829fa69d
ChenYn <highestop@qq.com>
2024-07-18 16:03:56 +0800

    promise can be aborted

--- src/promise/promise.ts
+++ src/promise/promise.ts
@@ -5,21 +5,31 @@ export function promise<T>(): Readonly<{
     resolve: (t: T) => void
     // reject to complete
     reject: (e?: Error) => void
+    // abort promise, no resolve or reject would happen
+    abort: () => void
     // if completed
     completed: boolean
 }> {
-    let resolve: (t: T) => void = () => { }
-    let reject: (e?: Error) => void = () => { }
+    let resolve: (t: T) => void = () => void 0
+    let reject: (e?: Error) => void = () => void 0
     let completed = false
+    let aborted = false
+    let abort = () => aborted = true
     const promise = new Promise<T>((_resolve, _reject) => {
         resolve = (t: T) => {
+            if (aborted) {
+                return
+            }
             _resolve(t)
             completed = true
         }
         reject = (e?: Error) => {
+            if (aborted) {
+                return
+            }
             _reject(e)
             completed = true
         }
     })
-    return Object.freeze({ promise, resolve, reject, completed })
+    return Object.freeze({ promise, resolve, reject, abort, completed })
 }
